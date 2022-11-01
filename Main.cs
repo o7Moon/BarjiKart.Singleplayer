5006b9049cab78451a6423d4d1ff4bdc4c9a62f0
o7Moon <0167moon@gmail.com>
2022-11-01 17:46:39 -0500

    port to melonloader

---
+++ Main.cs
@@ -0,0 +1,19 @@
+﻿using MelonLoader;
+using HarmonyLib;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using Il2CppSystem;
+
+[assembly: MelonInfo(typeof(BarjiKart.Singleplayer.SingleplayerMod), "BarjiKart.Singleplayer","1.0.0","o7Moon")]
+namespace BarjiKart.Singleplayer {
+    [HarmonyPatch]
+    public class SingleplayerMod : MelonMod
+    {
+        [HarmonyPatch(typeof(Barji.TrackSelectionManager),nameof(Barji.TrackSelectionManager.UpdateTrackSelectionGraphic))]
+        [HarmonyPostfix]
+        public static void onTrackSelect(Barji.TrackSelectionManager __instance, ushort __0, short __1, int __2){
+            Debug.Log("A");
+            if (Input.GetKey(KeyCode.LeftShift)) SceneManager.LoadScene(__1);
+        }
+    }
+}
